e1b60ba9870de6f7b698082c9878f5b60d6107af
ewan <ivan.stereotekk@gmail.com>
2023-01-06 01:25:13 +0300

    Variables Schema Added

--- dags/currencies_extractor.py
+++ dags/currencies_extractor.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-import datetime
+from airflow.models import Variable
 
 import os
 from airflow.utils.task_group import TaskGroup
@@ -9,39 +9,44 @@ from airflow.models import Variable
 from datetime import datetime, timedelta
 
 
-# constants
-MY_NAME = "IVAN"
-MY_AGE = 43
-CURR = 'RUB'
+now_stamp = datetime.now()
 
-URL_BTC = 'https://blockchain.info/ticker'
+# constants in Variables
+MY_NAME = Variable.get("my_name")
+MY_AGE = Variable.get("my_age")
+CURR = Variable.get("currency")
+URL_BTC = Variable.get("url_bitcoin")
+SELL_OR_BUY = Variable.get("sell_buy")
+PHONE = Variable.get("phone")
 
 
 def extract_currencies(curr, ti):
     import requests
     import json
-    URL_BTC = 'https://blockchain.info/ticker'
-
     res_btc = requests.get(URL_BTC)
     response_btc = json.loads(res_btc.text)
-    NEW_SNAP = response_btc['RUB']['buy']
-    ti.xcom_push(key=curr, value=NEW_SNAP)  # PUSHED Xcom
+    NEW_SNAP = response_btc[CURR][SELL_OR_BUY]
+    # PUSHED Xcom
+    ti.xcom_push(
+        key=curr, value=f"{NEW_SNAP} - {now_stamp} -- {extract_currencies.__name__} -- {MY_NAME} -- {PHONE}")
 
 
 def put_to_db(curr, ti):
-    pulled_data = ti.xcom_pull(key=curr, task_ids='put_data_to_db')
+    pulled_data = ti.xcom_pull(key=curr)
     ti.xcom_push(key="Latest_XCOM_PUSH", value=pulled_data)
     for_bash = f"Extracted BTC value is:{pulled_data}"
     print(f"Extracted BTC value is:{pulled_data}")
 
 
 def explore_data(curr, ti):
-    data = ti.xcom_pull(key="Latest_XCOM_PUSH", task_ids='explore_data')
-    print(f"I've done all tasks: {data}")
+    pulled_data = ti.xcom_pull(key=f"Latest_XCOM_PUSH")
+    print(f"I've done all tasks: {pulled_data}")
+    ti.xcom_push(
+        key=curr, value=f"{pulled_data} - {now_stamp} - {explore_data.__name__}")
 
 
 with DAG(
-    dag_id="extract_currencies_1",
+    dag_id="extract_currencies",
     start_date=datetime(2023, 1, 4),
     schedule_interval=None,  # timedelta(minutes=1),
     catchup=False,
@@ -50,7 +55,8 @@ with DAG(
         "owner": MY_NAME,
         "retries": 1,
         "retry_delay": timedelta(minutes=1)
-    }
+    },
+    doc_md="The DAG that makes something super useful for all humanity !"
 ) as dag:
 
     task1 = PythonOperator(
@@ -73,4 +79,4 @@ with DAG(
     )
 
 
-task1 >> [task2, task3]
+task1 >> task2 >> task3
